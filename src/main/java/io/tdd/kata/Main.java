fd1fb0ecf9587f1c3c59002cdaef40a77b8e23f4
Wan <sa.wan@sap.com>
2019-11-26 14:42:47 +0800

    print result

--- src/main/java/io/tdd/kata/Main.java
+++ src/main/java/io/tdd/kata/Main.java
@@ -2,6 +2,7 @@ package io.tdd.kata;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FizzBuzz FB = new FizzBuzz();
+        System.out.println(FB.printFizzBuzz(101));
     }
 }
