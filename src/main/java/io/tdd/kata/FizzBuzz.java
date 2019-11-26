d62afd2ea68c3bdfb252910c2a237bfe71b05024
Zoubek Zhu <zoubek.zhu@sap.com>
2019-11-26 11:29:15 +0800

    Zoubek Zhu test push

---
+++ src/main/java/io/tdd/kata/FizzBuzz.java
@@ -0,0 +1,10 @@
+package io.tdd.kata;
+
+public class FizzBuzz {
+    public boolean case1 (int i){
+        return true ;
+    }
+    public boolean case2 (int i){
+        return true ;
+    }
+}
--- src/test/java/io/tdd/kata/FizzBuzzTest.java
+++ src/test/java/io/tdd/kata/FizzBuzzTest.java
@@ -1,12 +1,20 @@
 package io.tdd.kata;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
+
+    public FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public  void setup(){
+    }
     @Test
     public void testExample() {
-        assertEquals(1, 1);
+        assertTrue( fizzBuzz.case1(1) );
     }
 }
