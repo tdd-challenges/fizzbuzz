8d17b18d10bbf18b9e60c9c23ee025119eca8b4a
Zoubek Zhu <zoubek.zhu@sap.com>
2019-11-26 14:22:44 +0800

    Zoubek Zhu test push 4

--- src/test/java/io/tdd/kata/FizzBuzzTest.java
+++ src/test/java/io/tdd/kata/FizzBuzzTest.java
@@ -9,22 +9,22 @@ public class FizzBuzzTest {
     public FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    public void _given_3_return_Fizz() {
+    public void test_given_3_return_Fizz() {
         assertEquals( "Fizz",fizzBuzz.calculation(3));
     }
 
     @Test
-    public void _given_5_return_Buzz() {
+    public void test_given_5_return_Buzz() {
         assertEquals( "Buzz",fizzBuzz.calculation(5));
     }
 
     @Test
-    public void _given_15_return_FizzBuzz() {
+    public void test_given_15_return_FizzBuzz() {
         assertEquals( "FizzBuzz",fizzBuzz.calculation(15));
     }
 
     @Test
-    public void _given_17_return_FizzBuzz() {
+    public void test_given_17_return_FizzBuzz() {
         assertEquals( "17",fizzBuzz.calculation(17));
     }
 }
