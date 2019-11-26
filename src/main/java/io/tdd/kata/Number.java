77e31c7a8cbfba6947bf27b49cb2e893ea307de0
Alex Dong <alex.dong@sap.com>
2019-11-26 14:57:48 +0800

    3rd run

--- src/main/java/io/tdd/kata/Number.java
+++ src/main/java/io/tdd/kata/Number.java
@@ -17,11 +17,11 @@ public class Number {
     public List<String>  generateArray(){
         for(int i = 0; i < this.length; i ++){
             String value = this.arr.get(i);
-            if((Integer.parseInt(value) % 3 == 0) && (Integer.parseInt(value) % 5 == 0)){
+            if(Integer.parseInt(value) % 15 == 0){
                 this.arr.set(i, "FizzBuzz");
-            }else if(Integer.parseInt(value) % 5 == 0 && Integer.parseInt(value) % 3 != 0){
+            }else if(Integer.parseInt(value) % 5 == 0){
                 this.arr.set(i, "Buzz");
-            }else if(Integer.parseInt(value) % 3 == 0 && Integer.parseInt(value)% 5 != 0){
+            }else if(Integer.parseInt(value) % 3 == 0){
                 this.arr.set(i, "Fizz");
             }
         }
