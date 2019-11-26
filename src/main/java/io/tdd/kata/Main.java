ccd41d9dabe5b4540f21e95f6b6c0c1027caec91
Tina Zhu <tina.zhu01@sap.com>
2019-11-26 14:53:34 +0800

    Impmentation

--- src/main/java/io/tdd/kata/Main.java
+++ src/main/java/io/tdd/kata/Main.java
@@ -2,5 +2,36 @@ package io.tdd.kata;
 
 public class Main {
     public static void main(String[] args) {
+    FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+    for(int i = 1; i <= 100; i++) {
+    	System.out.print(fizzBuzzGame.challenge1(i)+",");
     }
+    
+    }
+}
+
+class FizzBuzzGame{
+	public FizzBuzzGame() {
+		
+	}
+	public String challenge1(int i) {
+		//String[] ns = new String[100];
+		String s = "";
+		//for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 != 0) {				
+				s = "Fizz";
+				//ns[i-1] = s;
+			}else if(i % 3 != 0 && i % 5 == 0) {
+				s = "Buzz";
+				//ns[i-1] = s;
+			}else if(i % 3 == 0 && i % 5 == 0) {
+				s = "FizzBuzz";
+				//ns[i-1] = s;
+			}else {
+				s = String.valueOf(i);
+			}
+		//}
+		return s;
+		
+	}
 }
--- src/test/java/io/tdd/kata/FizzBuzzTest.java
+++ src/test/java/io/tdd/kata/FizzBuzzTest.java
@@ -5,8 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+	
     @Test
     public void testExample() {
         assertEquals(1, 1);
+    }
+    
+    FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+    @Test
+    public void test_3_output_fizz() {
+    	assertEquals("Fizz", fizzBuzzGame.challenge1(3));
+    }
+    
+    @Test
+    public void test_5_output_buzz() {
+    	assertEquals("Buzz", fizzBuzzGame.challenge1(5));
+    }
+    
+    @Test
+    public void test_15_output_fizzbuzz() {
+    	assertEquals("FizzBuzz", fizzBuzzGame.challenge1(15));
+    }
+    
+    @Test
+    public void test_1_output_1() {
+    	assertEquals("1", fizzBuzzGame.challenge1(1));
     }
 }
