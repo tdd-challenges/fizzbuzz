6f5c98f8a62ca0c81ee87224d516243d5435635b
Alex Dong <alex.dong@sap.com>
2019-11-26 15:07:07 +0800

    enhance output

--- src/main/java/io/tdd/kata/Main.java
+++ src/main/java/io/tdd/kata/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Number number = new Number(100);
         List<String> arr = number.generateArray();
         for( int j = 0; j < 100; j ++){
-            System.out.println(arr.get(j));
+            System.out.print(j == 99 ? arr.get(j) : arr.get(j) + ',');
         }
     }
 }
