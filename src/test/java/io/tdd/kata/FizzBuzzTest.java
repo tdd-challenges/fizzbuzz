ef0997c8a2ca26bd544c8f41e5f531f153d3a70f
Rebecca Xu <rebecca.xu@sap.com>
2019-11-27 16:37:49 +0800

    modify test

--- src/test/java/io/tdd/kata/FizzBuzzTest.java
+++ src/test/java/io/tdd/kata/FizzBuzzTest.java
@@ -9,16 +9,16 @@ public class FizzBuzzTest {
 
     @Test
     public void _3_print() {
-        assertEquals("1,2,Fizz",FizzBuzz.printNum(3));
+        assertEquals("1, 2, Fizz",FizzBuzz.printNum(3));
     }
 
     @Test
     public void _5_print() {
-        assertEquals("1,2,Fizz,4,Buzz",FizzBuzz.printNum(5));
+        assertEquals("1, 2, Fizz, 4, Buzz",FizzBuzz.printNum(5));
     }
 
     @Test
     public void _15_print() {
-        assertEquals( "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz", FizzBuzz.printNum(15));
+        assertEquals( "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", FizzBuzz.printNum(15));
     }
 }
