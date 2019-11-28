2dfa8be3df6b56d5064ac558e7f1a843be48a85d
Rebecca Xu <rebecca.xu@sap.com>
2019-11-28 11:04:40 +0800

    add function of warping every 14 lines.

---
+++ src/main/java/io/tdd/kata/LineWarp14.java
@@ -0,0 +1,17 @@
+package io.tdd.kata;
+
+public class LineWarp14 {
+    public static String warp() {
+        String Line = FizzBuzz.printNum(100);
+        int length = FizzBuzz.printNum(100).length();
+        double row = Math.ceil(length / 14);
+        String WarpedString = "";
+        for (int i = 1; i < row; i++) {
+            WarpedString = WarpedString + Line.substring((14 * (i - 1)), (14 * i));
+            WarpedString = WarpedString + "\n";
+        }
+        WarpedString = WarpedString + "z";
+        return WarpedString;
+    }
+
+}
--- src/main/java/io/tdd/kata/Main.java
+++ src/main/java/io/tdd/kata/Main.java
@@ -2,6 +2,8 @@ package io.tdd.kata;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(FizzBuzz.printNum(100));
+//        System.out.println(FizzBuzz.printNum(100));
+        System.out.println(LineWarp14.warp());
     }
+
 }
